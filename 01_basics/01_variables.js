7513c141530d06e83f8cf65698e97ea4b2557053
Piyusha Patil <96219966+CaptainCoolPiyusha@users.noreply.github.com>
2024-06-08 16:05:44 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountNo = 124561
+let accountEmail = "piyusha@gmail.com"
+var accountName = "Piyusha"
+accountCity = "Bengluru"
+console.table([accountNo, accountEmail, accountName, accountCity]);
+//accountNo = 69
+if(accountNo>30)
+    {
+        var accountName = "Patil"
+    }
+
+console.table([accountNo, accountEmail, accountName, accountCity]);
+// so avoid using var in js
+// as it creates problem in case of block scope and function scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as new version
+
+//alert(2+3)
+
+console.log(3+3) 
+console.log("piyusha")
+
+let name = "Piyusha"
+let age = 20
+let isLoggedIn = false
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// boolean (true/false)
+// null ==> Its standalone value
+// undefined => 
+// symbol => unique
+// object 
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
